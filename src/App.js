f04201d06214fb6c93964bc39274af5b8af05b01
andy silva <andssilva.23@gmail.com>
2021-05-29 21:22:40 -0400

    feat: :rocket: Configure the project, create useState()

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [reverse, setReverse] = useState(false); //fala de apenas uma coisa de dentro do estado, apenas uma chave e seu valor. Ex: reverse(key): false(value)
+  const [counter, setCounter] = useState(0);
+  const reverseClass = reverse ? 'reverse' : '';
+
+  const handleClick = () => {
+    setReverse((prevState) => !prevState);
+  };
+
+  const handleIncrement = () => {
+    setCounter((prevState) => prevState + 1);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className={`App-logo ${reverseClass}`} alt="logo" />
+
+        <h1>Contador: {counter}</h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <button type="button" onClick={handleClick}>
+            Reverse {reverseClass}
+          </button>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>
+          <button type="button" onClick={handleIncrement}>
+            Increment
+          </button>
+        </p>
       </header>
     </div>
   );
 }
+
+/* class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.handleClick = this.handleClick.bind(this);
+
+    this.state = {
+      reverse: false,
+    };
+  }
+
+  handleClick = () => {
+    const { reverse } = this.state;
+    this.setState({ reverse: !reverse });
+  };
+
+  render() {
+    const { reverse } = this.state;
+    const reverseClass = reverse ? 'reverse' : '';
+
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className={`App-logo ${reverseClass}`} alt="logo" />
+          <button type="button" onClick={this.handleClick}>
+            Reverse {reverseClass}
+          </button>
+        </header>
+      </div>
+    );
+  }
+} */
 
 export default App;
