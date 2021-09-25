d844d8f520295cb0dda14d03e00bb055568406e6
Anderson Silva <andssilva.23@gmail.com>
2021-09-25 03:49:23 -0400

    feat: :rocket: create a new hook - part 2

--- src/templates/App/index.jsx
+++ src/templates/App/index.jsx
@@ -1,11 +1,31 @@
 /* eslint-disable */
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
+
+const isObjEqual = (objA, objB) => {
+  return JSON.stringify(objA) === JSON.stringify(objB);
+}
 
 const useFecth = (url, options) => {
   const [result, setResult] = useState(null);
   const [loading, setLoading] = useState(false);
+  const [shouldLoading, setShouldLoading] = useState(false);
+  const urlRef = useRef(url);
+  const optionsRef = useRef(options)
 
   useEffect(() => {
+    if (!isObjEqual(url, urlRef.current)) {
+      urlRef.current = url;
+      setShouldLoading(!shouldLoading);
+    };
+    if (!isObjEqual(options, optionsRef.current)) {
+      optionsRef.current = options;
+      setShouldLoading(!shouldLoading);
+    };
+  }, [url, options])
+
+  useEffect(() => {
+    let wait = false;
+    console.log('EFFECT', new Date().toLocaleString());
     setLoading(true);
 
     const fetchData = async () => {
@@ -13,27 +33,63 @@ const useFecth = (url, options) => {
       await new Promise(r => setTimeout(r, 3000)); // impede que a função seja chamada diretamente
 
       try {
-        const responseData = await fetch(url, options);
+        const responseData = await fetch(urlRef.current, optionsRef.current);
         const jsonResult = await responseData.json();
-        setResult(jsonResult);
-        setLoading(false);
+        if (!wait) {
+          setResult(jsonResult);
+          setLoading(false);
+        }
       } catch (err) {
+        if (!wait) setLoading(false);
         throw err;
       }
     };
 
     fetchData();
 
-  }, [url]);
+    return () => {
+      wait = true
+    };
+
+  }, [shouldLoading]);
 
   return [result, loading];
 };
 
 function Home() {
-  const [result, loading] = useFecth('https://jsonplaceholder.typicode.com/users');
+  const [postId, setPostId] = useState('');
+  const [result, loading] = useFecth('https://jsonplaceholder.typicode.com/users/' + postId, {
+    headers: {
+      abc: '2000000',
+    }
+  });
+
+  useEffect(() => {
+    console.log('ID do user', postId);
+  }, [postId])
+
+  const handleClick = (id) => {
+    setPostId(id);
+  }
 
   if (loading) return <h1>Loading...</h1>;
-  if (!loading && result) console.log(result);
+  if (!loading && result) {
+    return (
+      <div>
+        {result?.length > 0 ? (
+          result.map((elem) => (
+            <div key={`user-${elem.id}`} onClick={() => handleClick(elem.id)}>
+              <p>{elem.name}</p>
+            </div>
+          ))
+        ) : (
+          <div onClick={() => handleClick('')}>
+            <p>{result.name}</p>
+          </div>
+        )}
+      </div>
+    )
+  };
 
   return (
     <div>
