4ac6ded3da48b75b169592e04044463d9b36ad6f
Anderson Silva <andssilva.23@gmail.com>
2021-09-25 04:01:08 -0400

    feat: :rocket: create a new hook - part 3

--- src/templates/App/index.jsx
+++ src/templates/App/index.jsx
@@ -1,76 +1,21 @@
-/* eslint-disable */
-import React, { useEffect, useRef, useState } from 'react';
-
-const isObjEqual = (objA, objB) => {
-  return JSON.stringify(objA) === JSON.stringify(objB);
-}
-
-const useFecth = (url, options) => {
-  const [result, setResult] = useState(null);
-  const [loading, setLoading] = useState(false);
-  const [shouldLoading, setShouldLoading] = useState(false);
-  const urlRef = useRef(url);
-  const optionsRef = useRef(options)
-
-  useEffect(() => {
-    if (!isObjEqual(url, urlRef.current)) {
-      urlRef.current = url;
-      setShouldLoading(!shouldLoading);
-    };
-    if (!isObjEqual(options, optionsRef.current)) {
-      optionsRef.current = options;
-      setShouldLoading(!shouldLoading);
-    };
-  }, [url, options])
-
-  useEffect(() => {
-    let wait = false;
-    console.log('EFFECT', new Date().toLocaleString());
-    setLoading(true);
-
-    const fetchData = async () => {
-      // let wait = false;
-      await new Promise(r => setTimeout(r, 3000)); // impede que a função seja chamada diretamente
-
-      try {
-        const responseData = await fetch(urlRef.current, optionsRef.current);
-        const jsonResult = await responseData.json();
-        if (!wait) {
-          setResult(jsonResult);
-          setLoading(false);
-        }
-      } catch (err) {
-        if (!wait) setLoading(false);
-        throw err;
-      }
-    };
-
-    fetchData();
-
-    return () => {
-      wait = true
-    };
-
-  }, [shouldLoading]);
-
-  return [result, loading];
-};
+import { useEffect, useState } from 'react';
+import { useFecth } from './use-fetch';
 
 function Home() {
   const [postId, setPostId] = useState('');
   const [result, loading] = useFecth('https://jsonplaceholder.typicode.com/users/' + postId, {
     headers: {
       abc: '2000000',
-    }
+    },
   });
 
   useEffect(() => {
     console.log('ID do user', postId);
-  }, [postId])
+  }, [postId]);
 
   const handleClick = (id) => {
     setPostId(id);
-  }
+  };
 
   if (loading) return <h1>Loading...</h1>;
   if (!loading && result) {
@@ -88,8 +33,8 @@ function Home() {
           </div>
         )}
       </div>
-    )
-  };
+    );
+  }
 
   return (
     <div>
